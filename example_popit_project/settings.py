79e0651d6a1bcc756095367efca10038d2be59b6
Matthew Somerville <matthew@mysociety.org>
2011-12-23 14:07:27 +0000

    Set ROOTs.

--- example_popit_project/settings.py
+++ example_popit_project/settings.py
@@ -70,7 +70,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.normpath( os.path.join( path, '..', 'media_root') )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -81,7 +81,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.normpath( os.path.join( path, '..', 'collected_static') )
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
